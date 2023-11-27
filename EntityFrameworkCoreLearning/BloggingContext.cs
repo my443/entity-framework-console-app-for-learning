4541bff5e41adf515715ee075e4423c8a4903ef9
John van Dijk <fortyfour-three@teksavvy.com>
2023-11-27 05:58:28 -0500

    Add project files.

---
+++ EntityFrameworkCoreLearning/BloggingContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+
+namespace EntityFrameworkCoreLearning
+{
+    internal class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(
+                @"Server=CENTRAL;Database=Blogging;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ EntityFrameworkCoreLearning/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace EntityFrameworkCoreLearning
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            BloggingContext myBloggingContext = new BloggingContext();
+            Blog myBlog = new Blog { Url = "http://blogs.msdn.com/adonet" };
+
+            myBloggingContext.Add(myBlog);
+            myBloggingContext.SaveChanges();
+
+            IQueryable<Blog> query = getBlogEntries(myBloggingContext);
+
+            foreach (Blog blog in query)
+            {
+                Console.WriteLine(blog.Url);
+            }
+
+            // Display the number of command line arguments.
+            Console.WriteLine("This works now.");
+        }
+
+        static IQueryable<Blog> getBlogEntries(BloggingContext myBloggingContext) {
+
+            IQueryable<Blog> blogQuery =
+                from blog in myBloggingContext.Blogs
+                //where cust.City == "London"
+                select blog;
+
+            return blogQuery;
+        }
+    }
+}
+    
